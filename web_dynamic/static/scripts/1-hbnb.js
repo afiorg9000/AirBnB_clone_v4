26f8fdb1fbd11bac0657f9ce3f0ee658e8d28fc2
afiorg9000 <3533@holbertonschool.com>
2022-05-26 21:27:43 +0000

    added the dynamic folder; started the flask app; added the filters in amenities AKA task 0,1,2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('input[type="checkbox"]').change(function() {
+		let amenities = {};
+		if (this.checked) { // check if the checkbox is checked
+			amenities[$(this).attr('data-id')] = $(this).attr('data-name'); // if checked get id attribute value of the clicked element
+		} else {
+			delete amenities[$(this).attr('data-id')]; //if the checkbox is unchecked, you must remove the Amenity ID from the variable
+		}
+		console.log(amenities); //log to console wether checked or not
+		if (Object.keys(amenities).length >= 0) { //update the h4 tag inside the div Amenities with the list of Amenities checked
+			$("DIV.amenities H4").text(Object.values(amenities).join(', '));
+		}
+	});
+});
